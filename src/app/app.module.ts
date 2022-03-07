26c7b38bc2fd8728556000d5266faa59da7423be
Stefany Eduarda <ostefany20@gmail.com>
2022-03-07 16:33:45 -0300

    Versão servidor

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TelaInicialComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
